189ba603868af86e2059b20085baa99b38f28077
Tom Focareta <tjfocare@hotmail.com>
2020-01-26 21:25:29 +1030

    added csv results writer

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,6 @@
-import re
+import path, csv, re, random, sys, warnings
 
-import random
-
-import sys
-
-import warnings
+from datetime import datetime
 
 import numpy as np
 
@@ -12,16 +8,14 @@ from board import Board, Cell, TYPE
 
 from pathlib import Path, PureWindowsPath
 
-# warnings.simplefilter(action='ignore', category=FutureWarning)
-
 x = 0
 
 boards = np.array([[]])
 
-# data_folder = Path("__tests__")
-# test_file = data_folder / "sudoku.txt"
-# test = open(test_file, 'r').readlines()
-# test = open('__tests__/sudoku.txt', 'r').readlines()
+data_folder = path.Path("__tests__")
+test_file = data_folder / "sudoku.txt"
+test = open(test_file, 'r').readlines()
+test = open('__tests__/sudoku.txt', 'r').readlines()
 
 # filename = Path("__tests__/sudoku.txt")
 
@@ -31,51 +25,44 @@ boards = np.array([[]])
 
 # print(path_on_windows)
 
-# for line in test:
-#     if not re.match('[A-Za-z]+\s[0-9]*', line):
-#         if re.match('[\n]', line[-1]):
-#             grid_row = list(line)[:-1]
-#         else:
-#             grid_row = list(line)
-#         boards = np.append(boards, grid_row)
-#
-# boards = np.reshape(boards, (-1, 9))
+for line in test:
+    if not re.match('[A-Za-z]+\s[0-9]*', line):
+        if re.match('[\n]', line[-1]):
+            grid_row = list(line)[:-1]
+        else:
+            grid_row = list(line)
+        boards = np.append(boards, grid_row)
+
+boards = np.reshape(boards, (-1, 9))
 
 # max 50
-# debug = False
-debug = True
-if len(sys.argv) > 1:
-    # board_no = int(sys.argv[1])
-    debug = True
+# if len(sys.argv) > 1:
+#     board_no = int(sys.argv[1])
 # else:
-# board_no = 0
-# board_no = random.randint(0, 49)
+#     board_no = random.randint(0, 49)
 
 # print('puzzle no: ', board_no)
 
-# 0:9, 9:18, 18:27, 27:36
-# board = np.array(boards[board_no * 9: board_no * 9 + 9, :], int)
-
-grid = np.array([
-    [Cell(x), Cell(x), Cell(3), Cell(x), Cell(
-        2), Cell(x), Cell(6), Cell(x), Cell(x)],
-    [Cell(9), Cell(x), Cell(x), Cell(3), Cell(
-        x), Cell(5), Cell(x), Cell(x), Cell(1)],
-    [Cell(x), Cell(x), Cell(1), Cell(8), Cell(
-        x), Cell(6), Cell(4), Cell(x), Cell(x)],
-    [Cell(x), Cell(x), Cell(8), Cell(1), Cell(
-        x), Cell(2), Cell(9), Cell(x), Cell(x)],
-    [Cell(7), Cell(x), Cell(1), Cell(x), Cell(
-        x), Cell(x), Cell(x), Cell(x), Cell(8)],
-    [Cell(x), Cell(x), Cell(6), Cell(7), Cell(
-        x), Cell(8), Cell(2), Cell(x), Cell(x)],
-    [Cell(x), Cell(x), Cell(2), Cell(6), Cell(
-        x), Cell(9), Cell(5), Cell(x), Cell(x)],
-    [Cell(8), Cell(x), Cell(x), Cell(2), Cell(
-        x), Cell(3), Cell(x), Cell(x), Cell(9)],
-    [Cell(x), Cell(x), Cell(5), Cell(x), Cell(
-        1), Cell(x), Cell(3), Cell(x), Cell(x)],
-])
+# grid = np.array([
+#     [Cell(x), Cell(x), Cell(3), Cell(x), Cell(
+#         2), Cell(x), Cell(6), Cell(x), Cell(x)],
+#     [Cell(9), Cell(x), Cell(x), Cell(3), Cell(
+#         x), Cell(5), Cell(x), Cell(x), Cell(1)],
+#     [Cell(x), Cell(x), Cell(1), Cell(8), Cell(
+#         x), Cell(6), Cell(4), Cell(x), Cell(x)],
+#     [Cell(x), Cell(x), Cell(8), Cell(1), Cell(
+#         x), Cell(2), Cell(9), Cell(x), Cell(x)],
+#     [Cell(7), Cell(x), Cell(x), Cell(x), Cell(
+#         x), Cell(x), Cell(x), Cell(x), Cell(8)],
+#     [Cell(x), Cell(x), Cell(6), Cell(7), Cell(
+#         x), Cell(8), Cell(2), Cell(x), Cell(x)],
+#     [Cell(x), Cell(x), Cell(2), Cell(6), Cell(
+#         x), Cell(9), Cell(5), Cell(x), Cell(x)],
+#     [Cell(8), Cell(x), Cell(x), Cell(2), Cell(
+#         x), Cell(3), Cell(x), Cell(x), Cell(9)],
+#     [Cell(x), Cell(x), Cell(5), Cell(x), Cell(
+#         1), Cell(x), Cell(3), Cell(x), Cell(x)],
+# ])
 
 
 def is_in_row(bo, num, row):
@@ -147,14 +134,11 @@ def find_next_empty(bo):
     return False
 
 
-max_row = 0
-max_col = 0
-
+steps = 0
 
 def solve_board(bo):
+    global steps_taken
     next_empty = find_next_empty(bo)
-    global max_row
-    global max_col
 
     if not next_empty:
         print('winner')
@@ -163,20 +147,13 @@ def solve_board(bo):
     else:
         row, col = next_empty
 
-    # print(next_empty)
-
     # try all numbers
     for testVal in range(1, 10):
+        steps_taken = steps_taken + 1
         if is_valid_placement(bo, testVal, row, col):
             # update board with new valid value
             bo.set_cell(testVal, row, col)
 
-            print('placing ' + str(testVal) + ' in : ' + str(row) + ', ' + str(col))
-            if row > max_row:
-                max_row = row
-            if col > max_col:
-                max_col = col
-            # print_board(bo)
             # check if board is solved
             if solve_board(bo):
                 return True
@@ -204,17 +181,58 @@ def print_board(bo):
             row_string += '\n  ' + '- ' * 12
         print(row_string)
 
-
 def main():
-    global max_row
-    global max_col
+    global steps
     board = Board(grid)
+
+    start_time = datetime.now()
 
     print('Sudoku...')
     print_board(board)
-
+    print('solving...')
     solve_board(board)
-    print(max_row, max_col)
+
+    end_time = datetime.now()
+    time_diff = (end_time - start_time) #.strftime('%H:%M:%S')
+    print('Solved in ' + str(time_diff) + ' with '  + str(steps) + ' steps.')
+
+def write_results():
+    global steps_taken
+
+    with open('test_results.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(
+            csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        csv_writer.writerow(['Puzzle no.', 'Time to complete', 'No. of steps'])
+
+        for no in range(0, 4):
+            board_no = no
+            # 0:9, 9:18, 18:27, 27:36
+            integer_board = np.array(boards[board_no * 9: board_no * 9 + 9, :], int)
+
+            # setup grid
+            grid = []
+            for i in range(0, 9):
+                row = []
+                for j in range(0, 9):
+                    value = integer_board[i][j]
+                    row.append(Cell(value))
+                grid.append(row)
+            grid = np.array(grid)
+
+            steps_taken = 0
+            board = Board(grid)
+
+            start_time = datetime.now()
+            print_board(board)
+            print('solving... puzzle: ', board_no)
+            solve_board(board)
+
+            end_time = datetime.now()
+            time_diff = (end_time - start_time) #.strftime('%H:%M:%S')
+
+            csv_writer.writerow([board_no, str(time_diff), steps_taken])
 
 
-main()
+# main()
+write_results()
